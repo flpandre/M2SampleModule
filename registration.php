2ee0b4a9c8735b357588fa6cd3cf1479cacda1e1
Andre Santos <andre@mediotype.com>
2021-09-24 11:42:34 -0300

    Create module structure

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Andre Santos <flp_andre@yahoo.com.br>
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Andre_M2Sample',
+    __DIR__
+);
