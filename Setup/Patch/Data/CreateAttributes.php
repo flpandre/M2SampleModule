a597c840abb745e6a4e0a1a9eed7d4326f15e506
Andre Santos <andre@mediotype.com>
2021-09-24 11:59:41 -0300

    Create product attributes

---
+++ Setup/Patch/Data/CreateAttributes.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * @author Andre Santos <flp_andre@yahoo.com.br>
+ */
+declare(strict_types=1);
+
+namespace Andre\M2Sample\Setup\Patch\Data;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
+use Magento\Catalog\Model\Product;
+
+class CreateAttributes implements DataPatchInterface
+{
+    /** @var ModuleDataSetupInterface */
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    /** @var EavSetupFactory */
+    private EavSetupFactory $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Create attributes
+     */
+    public function apply(): void
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(
+            ['setup' => $this->moduleDataSetup]
+        );
+        $newAttributes = $this->getAttributes();
+
+        foreach ($newAttributes as $code => $attribute) {
+            $eavSetup->addAttribute(
+                Product::ENTITY,
+                $code,
+                $attribute
+            );
+        }
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * Get attributes data
+     *
+     * @return array[]
+     */
+    private function getAttributes(): array
+    {
+        return [
+            'nickname' => [
+                'type' => 'text',
+                'frontend' => '',
+                'label' => 'Nick Name',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'backend' => '',
+                'global' => Attribute::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => false,
+                'is_wysiwyg_enabled' => false,
+                'unique' => false,
+                'apply_to' => ''
+            ],
+            'prop65' => [
+                'type' => 'int',
+                'frontend' => '',
+                'label' => 'Custom Prop',
+                'input' => 'boolean',
+                'class' => '',
+                'source' => '',
+                'backend' => '',
+                'global' => Attribute::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => false,
+                'is_wysiwyg_enabled' => false,
+                'unique' => false,
+                'apply_to' => ''
+            ]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies() : array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases() : array
+    {
+        return [];
+    }
+}
